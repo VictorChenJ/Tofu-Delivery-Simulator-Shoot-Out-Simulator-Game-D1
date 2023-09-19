678a0935371f7305990c042d19b253028a81a433
VictorChen <52492685+VictorChenJ@users.noreply.github.com>
2023-09-19 09:10:40 +0200

    Bullets
    Bullets virker, og man kan dø og miste liv

--- Enemy.gd
+++ Enemy.gd
@@ -1,9 +1,13 @@
-extends RigidBody2D
-
+extends KinematicBody2D
+ 
+# I like to have these scenes start with obj_, to distinguish them from other similarly named variables
 const obj_bullet = preload("res://bullet.tscn")
-
+ 
 func shoot(direction: float, speed: float):
 	var new_bullet = obj_bullet.instance()
 	new_bullet.velocity = Vector2(speed, 0).rotated(deg2rad(direction))
 	new_bullet.position = position
 	get_parent().add_child(new_bullet)
+
+func _process(delta):
+	shoot(10, 100)
