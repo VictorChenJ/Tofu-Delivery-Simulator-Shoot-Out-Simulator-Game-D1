66105625c60e4f6a656b905796d120985bf062f1
Pain <ebbemandsen@gmail.com>
2023-09-20 09:35:31 +0200

    enemy aiming fix

--- Scripts/NecoEnemy.gd
+++ Scripts/NecoEnemy.gd
@@ -1,55 +1,50 @@
 extends KinematicBody2D
- 
-#Enemy health
-var Ehp = 10 setget set_Ehp
 
+# Enemy health
+var Ehp = 10 setget set_Ehp
 signal Ehp_changed
 
-
-#Enemy movement
+# Enemy movement
 var speed = 200
 var motion = Vector2.ZERO
 var player = null
 
-
-#Bullet
+# Bullet
 const obj_bullet = preload("res://Scenes/bullet.tscn")
 var plpos = 0
 var timer = 0
-var time_interval = 1.5
+var time_interval = 1.0
 
-#Følger efter player
+# Følger efter player
 func _physics_process(delta):
 	motion = Vector2.ZERO
 	if player:
 		motion = position.direction_to(player.position) * speed
 	motion = move_and_slide(motion)
 
-#Bullet
+# Bullet
 func shoot(direction: float, speed: float):
 	var new_bullet = obj_bullet.instance()
 	new_bullet.velocity = Vector2(speed, 0).rotated(deg2rad(direction))
 	new_bullet.position = position
 	get_parent().add_child(new_bullet)
 	$GunshotSoundPlayer.play()
-	
 
 func _process(delta):
 	timer += delta
-	
+
 	if player:
+		_on_Player_playerposition(player.position)
 		if timer >= time_interval:
-			shoot(plpos, 800)
+			shoot(plpos, 400)
 			timer = 0
 
 func _ready():
 	connect("playerposition", self, "_on_Player_playerposition")
-	
+
 func _on_Player_playerposition(val):
-	plpos=rad2deg(get_angle_to(val))
+	plpos = rad2deg(get_angle_to(val))
 	pass # Replace with function body.
-
-
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("players"):
@@ -57,23 +52,22 @@ func _on_Area2D_body_entered(body):
 		player = body
 	pass # Replace with function body.
 
-
 func _on_Area2D_body_exited(body):
-	if body.is_in_group("players"):
+	if body.is_in_group("players") and body == player:
 		print("exit")
 		player = null
 	pass # Replace with function body.
 
-#Enemy health
-func take_damage ( dmg ):
+# Enemy health
+func take_damage(dmg):
 	set_Ehp(Ehp - dmg)
 
-func set_Ehp( new_Ehp ):
+func set_Ehp(new_Ehp):
 	emit_signal("Ehp_changed", new_Ehp)
 	Ehp = new_Ehp
 	if Ehp <= 0:
 		die()
-		
+
 func die():
 	emit_signal("e_died")
 	queue_free()
