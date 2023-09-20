23ca7b20d66a2129a5492e0f2cb167b185c7da16
mrcopycat25 <mrcopycat25@gmail.com>
2023-09-20 09:52:01 +0200

    health is killing me send help while im going to a therapist

---
+++ health.gd
@@ -0,0 +1,22 @@
+extends Label
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+
+	connect("hp_changed", self, "hpamount")
+	pass # Replace with function body.
+func _draw():
+	pass
+func hpamount(hp):
+	print("cheeese " + hp)
+	get_node("/root/MarginContainer/NinePatchRect/label").text=hp
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
