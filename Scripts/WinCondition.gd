3291f410584edd9e192b5c8860b83d309461cbff
Jfbt2013 <46356135+Jfbt2013@users.noreply.github.com>
2023-09-21 15:00:16 +0200

    You can make it big, in one night
    and he sure did

--- Scripts/WinCondition.gd
+++ Scripts/WinCondition.gd
@@ -14,5 +14,5 @@ func _on_Player_tofuamount(val):
 	winvar = winvar + val
 	#print("sssssssssss")
 	if winvar==get_child_count()&&val==0:
-		get_tree().quit()
+		get_tree().change_scene("res://Scenes/WinScreen.tscn")
 	pass # Replace with function body.
