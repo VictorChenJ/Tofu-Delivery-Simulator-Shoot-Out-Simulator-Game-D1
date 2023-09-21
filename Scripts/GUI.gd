c8e73757c59d6af273c2719c7ade91d75b37e2db
VictorChen <52492685+VictorChenJ@users.noreply.github.com>
2023-09-21 10:12:02 +0200

    dctryfkgjhbn

--- Scripts/GUI.gd
+++ Scripts/GUI.gd
@@ -3,7 +3,7 @@ extends Control
 func _ready():
 	connect("hp_changed", self, "_on_hp_changed")
 
-func _on_KinematicBody2D_hp_changed(new_hp):
+func _on_hp_changed(new_hp):
 	var progress_bar = $HealthBar  # Replace with your actual progress bar path
 	progress_bar.value = new_hp
 	print("hej")
--- Scripts/delivery.gd
+++ Scripts/delivery.gd
@@ -4,10 +4,11 @@ var tofuAmount = 0
 
 func _ready():
 	connect("tofuamount", self, "_on_tofu_amount_changed")
-func _on_Player_tofuamount(new_tofu_amount):
+	
+func _on_tofu_amount_changed(new_tofu_amount):
 	print("Tofu amount changed to:", new_tofu_amount)
 	tofuAmount = new_tofu_amount
-	pass # Replace with function body.
+
 func _on_delivery_body_entered(body):
 	if tofuAmount >= 1:
 		if body.is_in_group("players"):
@@ -15,3 +16,4 @@ func _on_delivery_body_entered(body):
 			emit_signal("tofu_delivered")
 		queue_free()
 	pass # Replace with function body.
+
