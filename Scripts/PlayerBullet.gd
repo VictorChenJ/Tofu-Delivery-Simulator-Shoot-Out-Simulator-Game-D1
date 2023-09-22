d8413b4fee054dfe5a18aa101b5b9bea8125860c
Pain <ebbemandsen@gmail.com>
2023-09-22 12:12:46 +0200

    bullet collision

--- Scripts/PlayerBullet.gd
+++ Scripts/PlayerBullet.gd
@@ -19,12 +19,9 @@ func _process(delta: float) -> void:
  
  
 func _on_body_entered(body):
-	# "body" here is the thing that we've hit
-	# Here we check if the body is a player, so we know to deal damage to them
-	# There are other ways to do this including class names and collision layers
-	if body.is_in_group("enemies"):
-		# You need to make sure your player has a "take_damage" function
-		body.take_damage(1)
+	if !body.is_in_group("players"):
+		if body.is_in_group("enemies"):
+			body.take_damage(1)
 		var richochetEffectInst = richochet_effect.instance()
 		var world = get_tree().current_scene
 		world.add_child(richochetEffectInst)
--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -16,15 +16,11 @@ func _process(delta: float) -> void:
 		queue_free()
 
 func _on_body_entered(body):
-	# "body" here is the thing that we've hit
-	# Here we check if the body is a player, so we know to deal damage to them
-	# There are other ways to do this including class names and collision layers
 	if body.is_in_group("players"):
-		# You need to make sure your player has a "take_damage" function
 		body.take_damage(1)
-		var RichochetEffectsInst = Ricochet_effect.instance()
-		var world = get_tree().current_scene
-		world.add_child(RichochetEffectsInst)
-		RichochetEffectsInst.global_position = global_position
-		RichochetEffectsInst.rotate(rotation)
-		queue_free()
+	var RichochetEffectsInst = Ricochet_effect.instance()
+	var world = get_tree().current_scene
+	world.add_child(RichochetEffectsInst)
+	RichochetEffectsInst.global_position = global_position
+	RichochetEffectsInst.rotate(rotation)
+	queue_free()
