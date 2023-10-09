5a547eccb699a69e74d76806d811ab1f7d2ef318
Pain <ebbemandsen@gmail.com>
2023-10-09 12:57:39 +0200

    sejt

--- Scripts/MusicPlayer.gd
+++ Scripts/MusicPlayer.gd
@@ -2,10 +2,13 @@ extends Node
 
 var rng = RandomNumberGenerator.new()
 var random_number
+var last_number
 
 func _process(delta):
 	if !$DynamicAudioPlayer.playing:
 		rng.randomize()
 		random_number = rng.randi_range(0, 14)
-		$DynamicAudioPlayer.stream = load("res://asset/music/" + str(random_number) + ".mp3")
-		$DynamicAudioPlayer.play()
+		if(random_number != last_number):
+			$DynamicAudioPlayer.stream = load("res://asset/music/" + str(random_number) + ".mp3")
+			$DynamicAudioPlayer.play()
+			last_number = random_number
