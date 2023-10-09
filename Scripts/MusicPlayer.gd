1fe6ceb8c899a700ca9ab1a283f9fdf6c9b7dd0f
Pain <ebbemandsen@gmail.com>
2023-10-09 13:11:28 +0200

    random music

--- Scripts/MusicPlayer.gd
+++ Scripts/MusicPlayer.gd
@@ -7,7 +7,7 @@ var last_number
 func _process(delta):
 	if !$DynamicAudioPlayer.playing:
 		rng.randomize()
-		random_number = rng.randi_range(0, 14)
+		random_number = rng.randi_range(0, 13)
 		if(random_number != last_number):
 			$DynamicAudioPlayer.stream = load("res://asset/music/" + str(random_number) + ".mp3")
 			$DynamicAudioPlayer.play()
