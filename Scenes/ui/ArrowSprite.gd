4751874dec3fcae2916e622bef37c6a457ceb3f0
mrcopycat25 <mrcopycat25@gmail.com>
2023-10-10 09:24:49 +0200

    Takumi i want you to know. I FUCKING FIXED THE ARROW LETS GOOOOOOO

---
+++ Scenes/ui/ArrowSprite.gd
@@ -0,0 +1,25 @@
+extends Sprite
+#onready var sprite=$Arrow
+var test=0
+#
+## Declare member variables here. Examples:
+## var a = 2
+## var b = "text"
+#
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass # Replace with function body.
+#func _process(delta):
+#	test=test+0.5
+#	print (rotation)
+#	rotation=test
+#
+#func set_marker_rotation():
+#	var spriteangle =(global_position-sprite.global_position).angle()
+#	rotation=test
+#	print (rotation)
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+##func _process(delta):
+##	pass
