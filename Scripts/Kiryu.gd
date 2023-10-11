5aec171a5cf140adaa95e848ae1ce173e9dc92cf
Jfbt2013 <46356135+Jfbt2013@users.noreply.github.com>
2023-10-11 18:13:02 +0200

    patch
    yes

--- Scripts/Kiryu.gd
+++ Scripts/Kiryu.gd
@@ -2,8 +2,8 @@ extends "res://Scripts/Enemy.gd"
 
 func _ready():
 	Ehp = 50
-	attack_speed = 100
-	speed = 300
+	attack_speed = 10
+	speed = 150
 	
 func _process(delta):
 	timer += attack_speed
--- Scripts/MapSelection.gd
+++ Scripts/MapSelection.gd
@@ -1,13 +1,6 @@
 extends CanvasLayer
 
 
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var MapSettings= get_node("/root/GlobalVar")
@@ -17,10 +10,6 @@ func _ready():
 		button.disabled=false
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 
 func _on_AkinaLevel_pressed():
