a0902102598de10d8870c8f2dc2bf6ee47c3161c
mrcopycat25 <mrcopycat25@gmail.com>
2023-11-01 14:18:20 +0100

    leaderboard works (but not really)
    Jan did something to his biscuits

--- Scripts/MapSelection.gd
+++ Scripts/MapSelection.gd
@@ -6,17 +6,16 @@ func _ready():
 	var MapSettings= get_node("/root/GlobalVar")
 	var button = get_node("CenterContainer/VBoxContainer/ShutokoLevel")
 	button.disabled = true
-	if(MapSettings.OAkinaPassed==true):
+	if(MapSettings.AkinaPassed==true):
 		button.disabled=false
-	pass # Replace with function body.
 
 
 
 func _on_AkinaLevel_pressed():
 	get_tree().change_scene("res://Scenes/maps/Akina.tscn")
-	pass # Replace with function body.
-
 
 func _on_ShutokoLevel_pressed():
 	get_tree().change_scene("res://Scenes/maps/Shutoko.tscn")
-	pass # Replace with function body.
+
+func _on_Leaderboard_pressed():
+	get_tree().change_scene("res://Scenes/menus/Leader_board.tscn")
