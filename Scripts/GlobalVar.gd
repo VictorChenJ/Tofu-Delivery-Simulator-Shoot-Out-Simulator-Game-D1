2ad1f5d6b7299797665a09d6e2cbb015b1660fb3
Jfbt2013 <46356135+Jfbt2013@users.noreply.github.com>
2023-11-01 13:39:50 +0100

    fix
    fix

--- Scripts/GlobalVar.gd
+++ Scripts/GlobalVar.gd
@@ -3,11 +3,11 @@ extends CanvasLayer
 export var AkinaPassed = false
 export var ShutokoPassed= false
 export var OAkinaPassed=false
-export var time_passed = ""
+export var time_passed = 0
 export var activeArrow = false
 export var Username=""
-export var AkinaTime=""
-export var ShutokoTime=""
+export var AkinaTime=0
+export var ShutokoTime=0
 
 # Declare member variables here. Examples:
 # var a = 2
--- Scripts/leaderboard scores.gd
+++ Scripts/leaderboard scores.gd
@@ -1,6 +1,6 @@
 extends Control
 
 func _ready():
-	var scoreboard = get_node("/root/Leader_board")
-	$leaderboard_board.hide()
+#	var scoreboard = get_node("/root/Leader_board")
+#	$leaderboard_board.hide()
 	
