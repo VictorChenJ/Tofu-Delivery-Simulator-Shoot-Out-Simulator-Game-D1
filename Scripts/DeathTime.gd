a76d7e9131cedbe28a40e4bec4038ffb7adb081b
Jfbt2013 <46356135+Jfbt2013@users.noreply.github.com>
2023-11-01 13:47:52 +0100

    more fix
    more fix

--- Scripts/DeathTime.gd
+++ Scripts/DeathTime.gd
@@ -2,4 +2,10 @@ extends Label
 
 func _ready():
 	var MapSettings= get_node("/root/GlobalVar")
-	text = MapSettings.time_passed 
+	var time=MapSettings.time_passed
+	var mils = fmod(time,1)*1000
+	var secs = fmod(time,60)
+	var mins = fmod(time, 60*60) / 60
+	
+	var time_passed = "%02d : %02d : %03d" % [mins,secs,mils]
+	text = time_passed 
